235d11a6c36643c66b93a9c3740749e25d40e037
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2023-09-25 21:36:12 +0200

    fix: Corregidas cards con varios nombres

---
+++ src/app/primera-letra-apellido.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PrimeraLetraApellidoPipe } from './primera-letra-apellido.pipe';
+
+describe('PrimeraLetraApellidoPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PrimeraLetraApellidoPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/primera-letra-apellido.pipe.ts
@@ -0,0 +1,18 @@
+import {
+  Pipe,
+  PipeTransform
+} from '@angular/core';
+
+@Pipe({
+  name: 'primeraLetraApellido'
+})
+export class PrimeraLetraApellidoPipe implements PipeTransform {
+
+  transform(apellido: string): string {
+    if (apellido && apellido.length > 0) {
+      return apellido[0];
+    } else {
+      return '';
+    }
+  }
+}
