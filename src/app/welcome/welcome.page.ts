d919832cc7e2495168fca40d1303b73a718506fa
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2023-10-01 13:25:11 +0200

    fix: Ajustes de console.log del usuario

--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -2,19 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-    selector: 'app-welcome',
-    templateUrl: './welcome.page.html',
-    styleUrls: ['./welcome.page.scss'],
-  }
-
-) export class WelcomePage implements OnInit {
-  nombre: string | null = ""
+  selector: 'app-welcome',
+  templateUrl: './welcome.page.html',
+  styleUrls: ['./welcome.page.scss'],
+})
+export class WelcomePage implements OnInit {
+  nombre: string | null = '';
 
   // Constructor de la página destino Welcome
-  constructor(private _route: ActivatedRoute) {}
+  constructor(private _route: ActivatedRoute) {
+    console.log(this._route.snapshot.paramMap.get('id'));
+  }
 
   ngOnInit() {
-    this.nombre = this._route.snapshot.paramMap.get('nombre')
-    console.log(this.nombre)
+    this.nombre = this._route.snapshot.paramMap.get('nombre');
+    console.log(this.nombre);
   }
 }
