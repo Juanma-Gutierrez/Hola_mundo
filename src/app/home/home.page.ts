73be73830b0f352a64b77e987861128d963f40a4
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2023-10-01 13:13:01 +0200

    feat: Añadido cambio de color al toast de favorito

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -17,29 +17,29 @@ export class HomePage implements OnInit {
   constructor(private router: Router, private toast: ToastController) {}
 
   public onFavClicked(user: User, event: UserInfoFavClicked) {
-    //recibimos en user el usuario asociado a la tarjeta
-    //recibimos en event un objeto del tipo UserInfoFavClicked que tiene una propiedad fav que indica si hay que añadir o eliminar de la lista de favoritos
-    //creamos una copia del array actual de usuarios
+    // recibimos en user el usuario asociado a la tarjeta
+    // recibimos en event un objeto del tipo UserInfoFavClicked que tiene una propiedad fav que indica si hay que añadir o eliminar de la lista de favoritos
+    // creamos una copia del array actual de usuarios
     const users = [...this._users.value];
-    //buscamos el índice del usuario para modificar su propiedad fav
+    // buscamos el índice del usuario para modificar su propiedad fav
     var index = users.findIndex((_user) => _user.id == user.id);
-    if (index!=-1)
-      //actualizamos la propiedad fav con el valor que hemos recibido por el evento
-      users[index].favorito = event.fav ?? false; //en el caso de que fav sea undefined devolvemos falso.
-    //notificamos un nuevo array de usuarios para que se renderice en la plantilla
+    if (index != -1)
+      // actualizamos la propiedad fav con el valor que hemos recibido por el evento
+      users[index].favorito = event.fav ?? false; // en el caso de que fav sea undefined devolvemos falso.
+    // notificamos un nuevo array de usuarios para que se renderice en la plantilla
     this._users.next([...users]);
-    //Notificamos con un Toast que se ha pulsado
+    // Notificamos con un Toast que se ha pulsado
     const options: ToastOptions = {
-      message: `${users[index].nombre} ${users[index].apellidos} ${event.fav ? 'added' : 'removed'} ${
-        event.fav ? 'to' : 'from'
-      } favourites`, //mensaje del toast
-      duration: 1000, // 1 segundo
-      position: 'bottom', // el toast se situa en la parte inferior
-      color: 'danger', // color del toast
-      cssClass: 'fav-ion-toast', //Una clase que podemos poner en global.scss para configurar el ion-toast
+      message: `${users[index].nombre} ${users[index].apellidos} ${
+        event.fav ? 'added' : 'removed'
+      } ${event.fav ? 'to' : 'from'} favourites`, //mensaje del toast
+      duration: 1000,                             // 1 segundo
+      position: 'bottom',                         // el toast se situa en la parte inferior
+      color: event.fav ? 'success' : 'danger',    // color del toast, verde si pone fav y rojo si quita fav
+      cssClass: 'fav-ion-toast',                  // Clase que podemos poner en global.scss para configurar el ion-toast
     };
 
-    //creamos el toast y lo presentamos (es una promesa por eso el then)
+    // creamos el toast y lo presentamos (es una promesa por eso el then)
     this.toast.create(options).then((toast) => toast.present());
   }
 
